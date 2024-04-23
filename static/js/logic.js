51e707707ab0ae01881264846161f60ef8c76769
Cory Lingerfelt <cory.m.lingerfelt@gmail.com>
2024-04-22 20:49:44 -0500

    added final code

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,15 +7,8 @@ d3.json(url).then(function(earthquakeData){
     createFeatures(earthquakeData.features);
 });
 function markerSize(magnitude) {
-    if (magnitude <= 1) return 5;
-    else if (magnitude > 1 & magnitude <= 2) return 10;
-    else if (magnitude > 2 & magnitude <= 3) return 15;
-    else if (magnitude > 3 & magnitude <= 4) return 20;
-    else if (magnitude > 4 & magnitude <= 5) return 25;
-    else if (magnitude > 5 & magnitude <= 6) return 30;
-    else if (magnitude > 6 & magnitude <= 7) return 35;
-    else if (magnitude > 7 & magnitude <= 8) return 40;
-    else return 45;
+    size = magnitude*50000
+    return size;
 };
 //creating function to determine earthquake depth
 function color(depth){
@@ -30,7 +23,7 @@ function color(depth){
 function createFeatures(earthquakeData){
     //Creating Pop-ups with Discriptions and Data
     function onEachFeature(feature, layer) {
-        layer.bindPopup('<h3>Earthquake Location: ${feature.properties.place}</h3><hr><p>Earthquake Date: ${new Date(feature.properties.time)}</p><p>Earthquake Magnitude: ${feature.properties.mag}</p><p>Earthquake Depth(km): ${feature.geometry.coordinates[2]}</p></hr>');
+        layer.bindPopup(['<h3>Earthquake Location: ', String(feature.properties.place),'</h3><hr><p>Earthquake Date: ', String(new Date(feature.properties.time)),'<p>]'].join());
     };
     
     let earthquakes=L.geoJSON(earthquakeData, {
@@ -58,7 +51,7 @@ function createMap(earthquakes) {
     let earthquake = L.layerGroup(earthquakes);
     let baseMap = {"Street Map": streetMap, "Topographic Map": topography};
     let overlay = {Earthquakes: earthquakes};
-    let myMap = L.map("map", {center: [-104.858,36.912], zoom: 2.0, layers: [streetMap, earthquakes]});
+    let myMap = L.map("map", {center: [0,0], zoom: 2.0, layers: [streetMap, earthquakes]});
     let mapLegend = L.control({position:"bottomleft"});
         mapLegend.onAdd = function() {let div = L.DomUtil.create("div", "info legend")
         let depth = [50, 100, 150, 200, 250]};
